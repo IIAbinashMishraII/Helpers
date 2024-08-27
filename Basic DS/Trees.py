7025727457511c232e9bef00d9119804cfc99b74
IIAbinashMishraII <99abinash.mishra@gmail.com>
2024-08-27 09:30:04 +0530

    Automated commit on 2024-08-27 at 09:30:  4 files changed, 71 insertions(+), 1 deletion(-)

--- Basic DS/Trees.py
+++ Basic DS/Trees.py
@@ -1,5 +1,5 @@
 # 2^0 + 2^1 + 2^2 + ... + 2^h = (2^(h+1)) - 1, total no of nodes in a complete binary tree
-
+from queue import Queue
 
 res = []
 class TreeNode:
@@ -7,7 +7,7 @@ class TreeNode:
         self.data = data
         self.right = None
         self.left = None
-    
+        self.res = []
 
     # All defined here are DFS algorithms
     def inorder(self, root):
@@ -40,4 +40,18 @@ class TreeNode:
         self.preorder(root.left)
         self.preorder(root.right)
 
-    
+    # All defined here are BFS algorithms
+    def levelorder(self, root):
+        if root is None:
+            return
+        queue = Queue()
+        queue.put(root)
+        curr = None
+        while len(queue) > 0:
+            curr = queue.get()
+            self.res.append(curr.data)
+            if curr.left:
+                queue.put(curr.left)
+            if curr.right:
+                queue.put(curr.right)
+                
