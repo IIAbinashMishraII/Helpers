19c7472e7c46fdb1ecb9515cbee30762ec4e6aea
IIAbinashMishraII <99abinash.mishra@gmail.com>
2025-03-19 09:30:04 +0530

    Automated commit on 2025-03-19 at 09:30:  1 file changed, 1 insertion(+), 1 deletion(-)

--- Basic Algo/Recursion.py
+++ Basic Algo/Recursion.py
@@ -1,20 +1,39 @@
-# Find somme subtask that it can perform without calling itself, that's the basecase
+# def chceck_palindrome(i ,n):
+#     if i >= n // 2:
+#         return True
+#     if arr[i] != arr[n - i  - 1]:
+#         return False
+#     return chceck_palindrome(i+1, n)
 
-def factorial(n):
-    if n < 2:
-        return n
-    return n * factorial(n-1) 
+# arr = input()
+# print(chceck_palindrome(0, len(arr)))
 
-n = int(input())
-def solve(x):
-    if x == 0:
+
+# def get_all_subsequences(i, temp):
+#     if i >= n:
+#         print(temp)
+#         return
+#     temp.append(arr[i])
+#     get_all_subsequences(i+1, temp)
+#     temp.pop()
+#     get_all_subsequences(i+1,temp)
+
+# arr = list(map(int, input().split()))
+# n = len(arr)
+# get_all_subsequences(0, [])
+
+
+def get_all_subseq_whose_sum_is_k(i, temp):
+    if i >= n:
+        if sum(temp) == k:
+            print(temp)
         return
-    solve(x-1)
-    print(x)
-solve(n)
+    temp.append(arr[i])
+    get_all_subseq_whose_sum_is_k(i+1,temp)
+    temp.pop()
+    get_all_subseq_whose_sum_is_k(i+1,temp)
 
-
-
-
-def test_all():
-    print(factorial(5))
+arr = list(map(int, input().split()))
+n = len(arr)
+k = int(input())
+get_all_subseq_whose_sum_is_k(0, [])
